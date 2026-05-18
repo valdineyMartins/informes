349eeba089f16447a7e0ba43ddde7905b7e9fafe
valdineyMartins <valdineymsouza@gmail.com>
2026-05-18 20:52:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
              descricao: "Orientar vendedores e gerentes sobre produtos.", link:"https://drive.google.com/file/d/1GUGYjOeu5YKUdAfWhYYMZRDqF_MKkyiK/view?usp=sharing"},
         { titulo: "💰Campanha comisão em dobro - AkzoNobel(Coral) 01/04/2026 a 30/06/2026", 
             descricao: "⏳ Trabalho realizado com vendedores e gerentes, resultados são passados semanalmente.", link:"https://docs.google.com/spreadsheets/d/1XDDSj8fK5gEoOk4y3iGRkRIgyDed9_r0RCKhvO-v8Kk/edit?usp=sharing"},
-        { titulo: "Tratativas de vencimento",
+        { titulo: "📅 Tratativas de vencimento",
              descricao: "Acompanhamento de validade e ações para para dar foco na venda dos itens presentes na lista.", link:"https://docs.google.com/spreadsheets/d/1xM8XrGBQ-9TThbljdo6eVLXjAGVJMXSovUC5qmEGPag/edit?usp=sharing"},
         { titulo: "💡 Introdução e divugação de produtos", 
             descricao: "Dar foco á produtos recem chegados ao estoque.", link:"https://drive.google.com/file/d/1m0p7XW-AJTktHZCTJ3yZWRI3rbO2kFdA/view?usp=sharing"},
